4fe223d3b264bb78774c7a04d99095c466e19db0
NicoMaker <93321282+NicoMaker@users.noreply.github.com>
2023-04-06 20:48:47 +0200

    Add files via upload

---
+++ ParteJS.js
@@ -0,0 +1,49 @@
+function Statistiche() 
+{
+    window.location.href = "Statistiche.html";
+}
+
+function Estate2020() 
+{
+    window.location.href = "Estate2020.html";
+}
+
+function Autunno_Inverno2020_2021()
+{
+    window.location.href = "Autunno_Inverno2020-2021.html";
+}
+
+function Primavera2021()
+{
+    window.location.href = "Primavera2021.html";
+}
+
+function Estate2021()
+{
+    window.location.href = "Estate2021.html";
+}
+
+function Autunno_Inverno2021_2022()
+{
+    window.location.href = "Autunno_Inverno2021-2022.html";
+}
+
+function Primavera2022()
+{
+    window.location.href = "Primavera2022.html";
+}
+
+function Estate2022() 
+{
+    window.location.href = "Estate2022.html";
+}
+
+function Autunno_Inverno2022_2023()
+{
+    window.location.href = "Autunno_Inverno2022-2023.html";
+}
+
+function Primavera2023()
+{
+    window.location.href = "Primavera2023.html";
+}
