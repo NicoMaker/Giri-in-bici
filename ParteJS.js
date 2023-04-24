22cbb78811d8830ad86c2033d518256d4764be63
NicoMaker <93321282+NicoMaker@users.noreply.github.com>
2023-04-24 11:56:28 +0200

    Add files via upload

--- ParteJS.js
+++ ParteJS.js
@@ -5,65 +5,65 @@ function Statistiche()
 
 function Estate2020() 
 {
-    window.location.href = "Estate2020.html";
+    window.location.href = "./Estate/Estate2020.html";
 }
 
 function Autunno_Inverno2020_2021()
 {
-    window.location.href = "Autunno_Inverno2020-2021.html";
+    window.location.href = "./Autunno_Inverno/Autunno_Inverno2020-2021.html";
 }
 
 function Primavera2021()
 {
-    window.location.href = "Primavera2021.html";
+    window.location.href = "./Primavera/Primavera2021.html";
 }
 
 function Estate2021()
 {
-    window.location.href = "Estate2021.html";
+    window.location.href ="./Estate/Estate2021.html";
 }
 
 function Autunno_Inverno2021_2022()
 {
-    window.location.href = "Autunno_Inverno2021-2022.html";
+    window.location.href = "./Autunno_Inverno/Autunno_Inverno2021-2022.html";
 }
 
 function Primavera2022()
 {
-    window.location.href = "Primavera2022.html";
+    window.location.href = "./Primavera/Primavera2022.html";
 }
 
 function Estate2022() 
 {
-    window.location.href = "Estate2022.html";
+    window.location.href = "./Estate/Estate2022.html";
 }
 
 function Autunno_Inverno2022_2023()
 {
-    window.location.href = "Autunno_Inverno2022-2023.html";
+    window.location.href = "./Autunno_Inverno/Autunno_Inverno2022-2023.html";
 }
 
 function Primavera2023()
 {
-    window.location.href = "Primavera2023.html";
+    window.location.href = "./Primavera/Primavera2023.html";
 }
 
 function Statistiche2020()
 {
-    window.location.href = "Statistiche2020.html"
+    window.location.href = "./Statistiche/Statistiche2020.html"
 }
 
 function Statistiche2021()
 {
-    window.location.href = "Statistiche2021.html"
+    window.location.href = "./StatisticheStatistiche2021.html"
 }
 
 function Statistiche2022()
 {
-    window.location.href = "Statistiche2022.html"
+    window.location.href = "./Statistiche/Statistiche2022.html"
 }
 
 function Statistiche2023()
 {
-    window.location.href = "Statistiche2023.html"
+    window.location.href = "./Statistiche/Statistiche2023.html"
 }
