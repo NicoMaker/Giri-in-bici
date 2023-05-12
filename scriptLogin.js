1aa72d2752128c1b3acc30de15f19df96716d776
NicoMaker <93321282+NicoMaker@users.noreply.github.com>
2023-05-12 10:05:25 +0200

    Add files via upload

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,14 +1,13 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita il comportamento di default del form
-  
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Verifica se le credenziali sono corrette
-    if (username === "NicolaJacopo" && password === "Giri2023") {
-      window.location.href = "giri.html"; // Reindirizza alla pagina home.html
-    } else {
-      alert("Credenziali non valide. Riprova.");
-    }
-  });
-  
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Evita il comportamento di default del form
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Verifica se le credenziali sono corrette
+  if (username === "NicolaJacopo" && password === "Giri2023") {
+    window.location.href = "giri.html"; // Reindirizza alla pagina home.html
+  } else {
+    alert("Credenziali non valide. Riprova.");
+  }
+});
