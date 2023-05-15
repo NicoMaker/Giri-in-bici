6bd8bf0cf7360675fef45271794d6b0193ca773b
NicoMaker <93321282+NicoMaker@users.noreply.github.com>
2023-05-15 13:55:01 +0200

    Update scriptLogin.js

--- scriptLogin.js
+++ scriptLogin.js
@@ -19,3 +19,28 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
       alert("Nome utente o password non validi!");
     }
 });
+
+    // Genera un numero casuale compreso tra min e max
+    function getRandomNumber(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // Genera un colore casuale in formato RGB
+    function getRandomColor() {
+      var red = getRandomNumber(0, 255);
+      var green = getRandomNumber(0, 255);
+      var blue = getRandomNumber(0, 255);
+      return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+    // Imposta lo sfondo con un'immagine astratta
+    function setAbstractBackground() {
+      var container = document.getElementById('container');
+      var backgroundColor = getRandomColor();
+      var backgroundImage = `linear-gradient(45deg, ${getRandomColor()}, ${getRandomColor()})`;
+      container.style.backgroundColor = backgroundColor;
+      container.style.backgroundImage = backgroundImage;
+    }
+
+    // Chiamata alla funzione per impostare lo sfondo all'avvio
+    setAbstractBackground();
