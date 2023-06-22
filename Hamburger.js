9cfc5dcb93946b83cc66edb5fe0af52c35e95eab
NicoMaker <93321282+NicoMaker@users.noreply.github.com>
2023-06-22 15:07:25 +0200

    Add files via upload

--- Hamburger.js
+++ Hamburger.js
@@ -1,20 +1,20 @@
 const menu = document.querySelector(".menu");
-    const hamburger = document.querySelector(".hamburger");
-    const closeIcon = document.querySelector(".CloseIcon");
-    const menuIcon = document.querySelector(".MenuIcon");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".CloseIcon");
+const menuIcon = document.querySelector(".MenuIcon");
 
-    function toggleMenu() {
+function toggleMenu() {
 
-        if (menu.classList.contains("showMwnu")) {
-            menu.classList.remove("showMwnu");
-            closeIcon.style.display = "none";
-            menuIcon.style.display = "block"
-        }
-        else {
-            menu.classList.add("showMwnu");
-            closeIcon.style.display = "block";
-            menuIcon.style.display = "none";
-        }
+    if (menu.classList.contains("showMwnu")) {
+        menu.classList.remove("showMwnu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block"
     }
+    else {
+        menu.classList.add("showMwnu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
 
-    hamburger.addEventListener("click", toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
