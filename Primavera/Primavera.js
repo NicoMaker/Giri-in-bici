2a2e0334140bc7201d7b8c0a451b9da8a7f86201
Nico Maker <nicola.marano02@gmail.com>
2023-12-31 12:45:33 +0100

    Aggiunta primavera codice js ma non html

--- Primavera/Primavera.js
+++ Primavera/Primavera.js
@@ -1,5 +1,5 @@
-let p2021 = 579, p2022 = 885, p2023 = 742;
-let totale = p2021 + p2022 + p2023;
+let p2021 = 579, p2022 = 885, p2023 = 742, p2024 = 0;
+let totale = p2021 + p2022 + p2023 + p2024;
 
 let avg2021 = (p2021 / totale) * 100;
 avg2021 = parseFloat(avg2021.toFixed(2));
@@ -10,22 +10,27 @@ avg2022 = parseFloat(avg2022.toFixed(2));
 let avg2023 = (p2023 / totale) * 100;
 avg2023 = parseFloat(avg2023.toFixed(2));
 
+let avg2024 = (p2024 / totale) * 100;
+avg2024 = parseFloat(avg2024.toFixed(2));
+
 let dati = {
-    labels: ['2021', '2022', '2023'],
+    labels: ['2021', '2022', '2023', '2024'],
     datasets: [{
         label: 'km Primavera',
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
             'rgba(255, 206, 86, 0.2)',
+            '#97ed86ce'
         ],
         borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
+            'rgba(255, 206, 86, 1)',
+            'lightgreen'
         ],
         borderWidth: 1,
-        data: [p2021, p2022, p2023]  // Aggiunto un valore per il 2024, correggi se necessario
+        data: [p2021, p2022, p2023,p2024]  // Aggiunto un valore per il 2024, correggi se necessario
     }]
 };
 
@@ -76,10 +81,22 @@ let stampa = `
         </p>
     </a>
 </div>
+
+<!-- <div class="Primaveracontorno">
+    <img class="immaginestagione" src="Icone/primavera.png">
+    <a href="Primavera/Primavera2024.html">
+        <p class="titoli">
+            Primavera 2024
+
+            <p>Totale km <img src="Icone/traguardo.png"> ${p2024}</p> 
+            <p> ${avg2024} % </p>
+        </p>
+    </a>
+</div> -->
 `;
 document.getElementById("stampa").innerHTML = stampa;
 
-let avgp = totale/3;
+let avgp = totale/4;
 avgp = avgp.toFixed(2);
 
 let stampaP = `Totale km percorsi in Primavera ${totale} <img src="Icone/traguardo.png"> <p>media km per stagione ${avgp} </p>`;
