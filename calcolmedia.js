d7c002a6842b85f2558af73bd70aa531d239095c
Nico Maker <nicola.marano02@gmail.com>
2024-01-06 14:36:15 +0100

    Modfiica readme e tolgli funzione in calcolamedia

--- calcolmedia.js
+++ calcolmedia.js
@@ -1,11 +1,10 @@
-function media(){
-    let km = parseFloat(document.getElementById("km").getAttribute("data-km"));
-    let corse = parseFloat(document.getElementById("km").getAttribute("data-corse"));
+let km = parseFloat(document.getElementById("km").getAttribute("data-km"));
+let corse = parseFloat(document.getElementById("km").getAttribute("data-corse"));
 
-    let mediaValue = km / corse;
-    mediaValue = parseFloat(mediaValue.toFixed(2));
+let mediaValue = km / corse;
+mediaValue = parseFloat(mediaValue.toFixed(2));
 
-    let stampa = `
+let stampa = `
     <div class="colore">
         <p> totale km ${km}
             <img src="../Icone/traguardo.png">
@@ -14,7 +13,4 @@ function media(){
     </div>
     `;
 
-    document.getElementById("km").innerHTML = stampa;
-}
-
-media();
+document.getElementById("km").innerHTML = stampa;
--- readme.md
+++ readme.md
@@ -17,10 +17,18 @@
 - Nome Utente -> NicoMaker
 - Password -> Giri anno corrispondente
 
-# Apertura sito
+# Logo e Apertura sito
 
+![Logo](imagini%20Html%20sito%20principale/Logo%20.JPG)
 [Apertura sito principale](https://giri-in-bici.netlify.app/) 
 
 # Mappa Sito
 
 ![Mappa Sito](About_US/Mappa.jpg)
+
+# Avatar Partecipanti
+
+<div style="display: flex; justify-content: center; align-items: center;">
+    <img src="About_US/Avatar/AvatarNM.jpg" alt="Avatar" style="border-radius: 50%; margin: 5px;">
+    <img src="About_US/Avatar/AvatarJR.png" alt="Avatar" style="border-radius: 50%; margin: 5px;">
+</div>
