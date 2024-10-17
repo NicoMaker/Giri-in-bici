b1dccc5727e56606f8f01f9aa7aa3adda0509ebd
Nicola <93321282+NicoMaker@users.noreply.github.com>
2024-10-17 22:06:49 +0200

    Update scriptinfo.js

--- About_US/scriptinfo.js
+++ About_US/scriptinfo.js
@@ -1,6 +1,6 @@
-const info = `&copy; 30 / Maggio / 2020 - ${
+const info = `&copy; 30 Maggio 2020 - ${
   (new Date().getDate() < 10 ? "0" : "") + new Date().getDate()
-} / ${
+} ${
   [
     "Gennaio",
     "Febbraio",
@@ -15,6 +15,6 @@ const info = `&copy; 30 / Maggio / 2020 - ${
     "Novembre",
     "Dicembre",
   ][new Date().getMonth()]
-} / ${new Date().getFullYear()} 
+} ${new Date().getFullYear()} 
 <br>Nico Maker Giri in Bici`;
 document.getElementById("info").innerHTML = `<footer>${info}</footer>`;
