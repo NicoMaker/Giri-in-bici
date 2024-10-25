c4e417a44472e26ab0990ce0f01a4d30fa0e3e7e
NicoMaker <Nicola.marano02gmail.com>
2024-10-25 07:15:55 +0200

    ottimizazione js

--- JS/Calcolo_km_totali.js
+++ JS/Calcolo_km_totali.js
@@ -1,31 +1,33 @@
 fetch("Statistiche/Js/History/JSON/Generale.json")
   .then((response) => response.json())
   .then((data) => {
-    const { statistics } = data;
     let kmData = [],
       totaleCorse = 0;
 
-    const fetchPromises = Object.values(statistics).map((url) =>
-      fetch(url)
-        .then((response) => response.json())
-        .then((annoData) => {
-
-          if (annoData.data && typeof annoData.data === "object") {
-            const kmValues = Object.values(annoData.data);
-            kmData = [...kmData, ...kmValues];
-          } else
-            console.error("Dati dei chilometri mancanti o non validi in:", url);
-          if (typeof annoData.numberOfRaces === "number") {
-            totaleCorse += annoData.numberOfRaces;
-          } else console.error("numberOfRaces non è un numero valido in:", url);
-        })
-    );
+    const { statistics } = data,
+      fetchPromises = Object.values(statistics).map((url) =>
+        fetch(url)
+          .then((response) => response.json())
+          .then((annoData) => {
+            if (annoData.data && typeof annoData.data === "object") {
+              const kmValues = Object.values(annoData.data);
+              kmData = [...kmData, ...kmValues];
+            } else
+              console.error(
+                "Dati dei chilometri mancanti o non validi in:",
+                url
+              );
+            if (typeof annoData.numberOfRaces === "number") {
+              totaleCorse += annoData.numberOfRaces;
+            } else
+              console.error("numberOfRaces non è un numero valido in:", url);
+          })
+      );
 
     Promise.all(fetchPromises)
       .then(() => {
         const totaleKm = calcolaTotaleKm(kmData),
           mediaKm = calcolaMediaKm(totaleKm, totaleCorse);
-
 
         StampaDati(totaleKm, mediaKm);
       })
