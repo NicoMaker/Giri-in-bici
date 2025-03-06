6dbe6707c934bc85e578840429326d22b9800e38
NicoMaker <nicola.marano02gmail,com>
2025-03-06 21:28:57 +0100

    migloria codice

--- JS/Calcolo_km_totali.js
+++ JS/Calcolo_km_totali.js
@@ -8,50 +8,46 @@ const fetchJSON = (url) => fetch(url).then((response) => response.json()),
       console.error("Formato JSON non valido");
       return 0;
     }
-  };
+  },
+  processSeasons = (data) => {
+    let kmData = [],
+      totaleCorse = 0,
+      totalePeriodi = 0;
 
-const processSeasons = (data) => {
-  let kmData = [],
-    totaleCorse = 0,
-    totalePeriodi = 0;
-
-  const fetchPromises = data.seasons.flatMap((season) =>
-    Object.values(season.subPeriods).map((url) =>
-      fetchJSON(url).then((periodData) => {
-        totaleCorse += processPeriodData(periodData, kmData);
-        totalePeriodi++;
-      })
-    )
-  );
-
-  Promise.all(fetchPromises)
-    .then(() => {
-      const totaleKm = calcolaTotaleKm(kmData),
-        mediaKm = calcolaMediaKm(totaleKm, totaleCorse),
-        avgPeriod = calcolaMediaPeriodo(totaleKm, totalePeriodi);
-
-      StampaDati(totaleKm, mediaKm, avgPeriod);
-    })
-    .catch((error) =>
-      console.error(`Errore nel caricamento dei JSON: ${error}`)
+    const fetchPromises = data.seasons.flatMap((season) =>
+      Object.values(season.subPeriods).map((url) =>
+        fetchJSON(url).then((periodData) => {
+          totaleCorse += processPeriodData(periodData, kmData);
+          totalePeriodi++;
+        })
+      )
     );
-};
 
-const calcolaTotaleKm = (kmData) => kmData.reduce((total, km) => total + km, 0),
+    Promise.all(fetchPromises)
+      .then(() => {
+        const totaleKm = calcolaTotaleKm(kmData),
+          mediaKm = calcolaMediaKm(totaleKm, totaleCorse),
+          avgPeriod = calcolaMediaPeriodo(totaleKm, totalePeriodi);
+
+        StampaDati(totaleKm, mediaKm, avgPeriod);
+      })
+      .catch((error) =>
+        console.error(`Errore nel caricamento dei JSON: ${error}`)
+      );
+  },
+  calcolaTotaleKm = (kmData) => kmData.reduce((total, km) => total + km, 0),
   calcolaMediaKm = (totaleKm, corse) =>
     corse > 0 ? (totaleKm / corse).toFixed(2) : 0,
   calcolaMediaPeriodo = (totaleKm, periodi) =>
     periodi > 0 ? (totaleKm / periodi).toFixed(2) : "N/A",
-  StampaDati = (totaleKm, mediaKm, avgPeriod) => {
-    document.getElementById("km").innerHTML = `
+  StampaDati = (totaleKm, mediaKm, avgPeriod) =>
+    (document.getElementById("km").innerHTML = `
     <div class="colore">
       <p class="misuracolore">Totale km ${totaleKm} <img src="Icons/traguardo.png" alt="Icona traguardo"></p>
       <p class="misuracolore">km medi per giro ${mediaKm}</p>
       <p class="misuracolore">Media km per Periodo ${avgPeriod} km</p>
     </div>
-  `;
-  };
-
+  `);
 fetchJSON("Statistiche/Js/anni/stagioni.json")
   .then(processSeasons)
   .catch((error) =>
