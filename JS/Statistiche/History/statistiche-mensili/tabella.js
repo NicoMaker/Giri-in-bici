0cab2ba4ec9f015825ade6646feaa36beea6cacb
NicoMaker <nicola.marano02@gmail.com>
2026-08-07 14:40:46 +0200

    fix: update table header for monthly average kilometers and enhance responsive design for statistics table

--- JS/Statistiche/History/statistiche-mensili/tabella.js
+++ JS/Statistiche/History/statistiche-mensili/tabella.js
@@ -15,7 +15,7 @@ window.GraficoTotaleMensile = window.GraficoTotaleMensile || {};
       <th>km <img src="/img/Icons/traguardo.png"></th>
       <th>Percentuale sul totale</th>
       <th>Mesi di Corsa</th>
-      <th>km <img src="/img/Icons/traguardo.png"> medi</th>
+      <th>km <img src="/img/Icons/traguardo.png"> medi mensili</th>
      </tr>
     ${kmPerMese
       .map(
